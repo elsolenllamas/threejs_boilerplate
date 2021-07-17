d5b9edba1ec120d27c0e311a2bddd9af1e0f9483
elsolenllamas <elsolenllamas@gmail.com>
2021-07-17 12:52:20 -0300

    adding main js file

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+
+const gui = new dat.GUI()
+const scene = new THREE.Scene();
