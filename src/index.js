2dd340069bd7b30dffb3cda66a43313e81387754
elsolenllamas <elsolenllamas@gmail.com>
2021-08-01 17:36:18 -0300

    adding camera and renderer

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+const settings = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+const canvas = document.getElementById('canvas');
+
 const gui = new dat.GUI()
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(settings.width, settings.height);
+
+const clock = new THREE.Clock();
